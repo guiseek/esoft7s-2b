3e3841f83da9de9648beb8634a7c2a0cc878d693
Gui Seek <guiseek@gmail.com>
2023-05-26 17:19:50 -0300

    feat(dx): add dx features

--- apps/dev/src/app/app.tsx
+++ apps/dev/src/app/app.tsx
@@ -1,47 +1,10 @@
 import { Component } from 'react';
-
+import { DxFeatureShell } from '@esoft7s/dx/feature-shell';
 import './app.scss';
-
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
 
 export class App extends Component {
   render() {
-    return (
-      <div>
-        <div role="navigation">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/page-2">Page 2</Link>
-            </li>
-          </ul>
-        </div>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                This is the generated root route.{' '}
-                <Link to="/page-2">Click here for page 2.</Link>
-              </div>
-            }
-          />
-          <Route
-            path="/page-2"
-            element={
-              <div>
-                <Link to="/">Click here to go back to root page.</Link>
-              </div>
-            }
-          />
-        </Routes>
-        {/* END: routes */}
-      </div>
-    );
+    return <DxFeatureShell />;
   }
 }
 
