86ea2ef99cede28e4073014f84da780192233175
Gui Seek <guiseek@gmail.com>
2023-05-26 18:23:39 -0300

    feat(main-ui-player): add audio and icons

--- libs/main/ui-player/src/lib/ui-player.component.ts
+++ libs/main/ui-player/src/lib/ui-player.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'esoft7s-ui-player',
@@ -7,4 +13,31 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UiPlayerComponent {
+  @ViewChild('audioRef')
+  audioRef!: ElementRef<HTMLAudioElement>;
+  get audioEl() {
+    return this.audioRef.nativeElement;
+  }
+
+  #paused = new BehaviorSubject(true);
+  paused$ = this.#paused.asObservable();
+
+  #muted = new BehaviorSubject(false);
+  muted$ = this.#muted.asObservable();
+
+  play() {
+    if (this.audioEl.paused) {
+      this.audioEl.play();
+    } else {
+      this.audioEl.pause();
+    }
+
+    this.#paused.next(this.audioEl.paused);
+  }
+
+  mute() {
+    this.audioEl.muted = !this.audioEl.muted;
+
+    this.#muted.next(this.audioEl.muted);
+  }
 }
